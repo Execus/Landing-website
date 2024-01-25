579df70fab62eac811ad14c33754e247548d3264
Mikhail <115924723+Execus@users.noreply.github.com>
2024-01-25 16:37:19 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
-const owl = $('.owl-carousel');
+//Corousel
+
+const owl = $(".owl-carousel");
 
 owl.owlCarousel({
-	center: true,
-	loop: true,
-	margin: 30,
-	startPosition: 1,
-	items: 3,
+  center: true,
+  loop: true,
+  margin: 20,
+  startPosition: 1,
+  items: 3,
+  responsive: {
+    0: {},
+    850: {
+      startPosition: 0,
+      items: 3,
+    },
+    1000: {
+      margin: 20,
+      items: 3,
+    },
+    1200: {
+      margin: 30,
+    },
+  },
 });
 
-$('.slider__btn--prev').click(function () {
-	owl.trigger('prev.owl.carousel');
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
 });
 
-
-$('.slider__btn--next').click(function () {
-	owl.trigger('next.owl.carousel');
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
 });
+
+//Nov icon
+
+const navBtn = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  nav.classList.toggle("nav--mobile");
+  menuIcon.classList.toggle("menu-icon-active");
+  document.body.classList.toggle("no-scroll");
+};
